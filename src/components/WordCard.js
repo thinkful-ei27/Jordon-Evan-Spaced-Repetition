b1ac9a0138e1bb02a934f95beb2e09ccae6ae852
Evan Guirino <etg624@gmail.com>
2019-03-18 18:21:34 -0700

    made ugly form to submit answer to

--- src/components/WordCard.js
+++ src/components/WordCard.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { required } from '../validators';
+import Input from './input';
 function WordCard(props) {
-  return <div>{props.words}</div>;
+  return (
+    <div>
+      <header>{props.words}</header>
+      <form>
+        <Field
+          name="answerInput"
+          component={Input}
+          type="text"
+          placeholder="tu respuesta"
+          validate={[required]}
+        />
+        <button type="submit">envía tu respuesta</button>
+      </form>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
   words: state.words.words
 });
-export default connect(mapStateToProps)(WordCard);
+
+WordCard = connect(mapStateToProps)(WordCard);
+export default reduxForm({
+  form: 'submitAnswerForm'
+})(WordCard);
